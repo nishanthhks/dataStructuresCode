d86a5a69c48970434e0fc64a2283a6552ae93abc
Nishanth <nishanth.kkps@gmail.com>
2024-01-03 22:13:58 +0530

    modefied stack operations

--- unit-1-Stackcodes/stackOperations.c
+++ unit-1-Stackcodes/stackOperations.c
@@ -10,12 +10,12 @@ int stack[MAX];
 int main(){
 do
 {
-    printf("Enter options to perform operations\n1.push\n2.pop\n3.peep\n4.dispaly\n5.exit\n");
+    printf("\nEnter options to perform operations\n1.push\n2.pop\n3.peep\n4.dispaly\n5.exit\n");
     int option;
     scanf("%d",&option);
     switch (option)
     {
-        case 1 : printf("Enter the number to push : ");
+        case 1 : printf("\nEnter the number to push : ");
                  int num;
                  scanf("%d",&num);
                  push(num);
@@ -29,23 +29,23 @@ do
                  break;
         case 5 : exit(0);
                  break;
-        default : printf("enter a valid number");                                             
+        default : printf("\nenter a valid number");                                             
     }
 } while (1);    
 }
 
 void push(int element){
     if(top == MAX-1){
-        printf("stack is full");
+        printf("\nstack is full\n");
     }else{
         top++;
         stack[top] = element;
-        printf("element pushed");
+        printf("\nelement pushed\n");
     }
 }
 int pop(){
     if(top == -1){
-        printf("stack is empty");
+        printf("\nstack is empty\n");
         return -1;
     }else{
         int poppedEle = stack[top];
@@ -55,7 +55,7 @@ int pop(){
 }
 int peep(){
     if(top == -1){
-        printf("stack is empty");
+        printf("\nstack is empty\n");
         return -1;
     }else{
         return stack[top];
@@ -63,9 +63,9 @@ int peep(){
 }
 void display(){
     if(top == -1){
-        printf("stack is empty");
+        printf("\nstack is empty\n");
     }else{
-        for(int i = 0 ; i<MAX ; i++){
+        for(int i = 0 ; i<=top ; i++){
              printf("%d\t",stack[i]);
         }
     }
